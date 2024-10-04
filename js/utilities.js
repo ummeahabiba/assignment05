e7a8988b00945bbc5d3d7060a37909cc4e0a39e0
ummeahabiba <ummeahabiba12@gmail.com>
2024-10-04 09:31:56 +0600

    added index and blog connection abd other files

---
+++ js/utilities.js
@@ -0,0 +1,11 @@
+// redirect to home page
+
+function redirectToPageIndex(){
+    window.location.href = '/index.html'; 
+}
+
+// redirect to blog page
+
+function redirectToPageBlog(){
+    window.location.href = '/blog.html'; 
+}
